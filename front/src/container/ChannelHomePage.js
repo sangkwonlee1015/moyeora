9c55c40af00751130870aca1aabdeeb186944dd2
jy-lee0626 <lklll369@gmail.com>
2022-08-04 04:54:19 +0900

    Header_css

--- front/src/container/ChannelHomePage.js
+++ front/src/container/ChannelHomePage.js
@@ -1,13 +1,9 @@
-import { useParams } from 'react-router-dom';
-
 import Header from "../components/Header/Header"
 import Sidebar from "../components/Sidebar/Sidebar"
 import ChannelHome from "../components/ChannelHome"
 
 function ChannelHomePage(){
-    const a = useParams();
-    console.log(a);
-    
+
     return (
         <div className="comp">
             <Header/>
