6934999e9e74a8d3e8670701f47f087a02545f7e
2015004175 <2015004175@hanyang.ac.kr>
2022-08-03 23:58:58 +0900

    conflict 수정 및 login 수정

--- front/src/container/App.js
+++ front/src/container/App.js
@@ -1,25 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-import './App.css';
-import Homepage from './Homepage';
-import LoginPage from './LoginPage';
+import "./App.css";
+import Homepage from "./Homepage";
+import LoginPage from "./LoginPage";
 import Header from "../components/header/Header";
 import ChattingPage from "../components/Sidebar/Chatting";
 
 function App() {
-  let LoggedIn = true;
+  let LoggedIn = false;
   return (
     <div>
-      {LoggedIn ? <Header/> : null}
+      {LoggedIn ? <Header /> : null}
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={LoggedIn ? <Homepage/> : <LoginPage/>}/>
-          <Route path="/homepage" element={<Homepage/>}/>
-          <Route path="/:channelId/chattingpage" element={<ChattingPage/>}/>
-          <Route path="/:channelId/mappage/" element={<Homepage/>}/>
-          <Route path="/home" element={<Homepage/>}/>
-          <Route path="/home" element={<Homepage/>}/>
-          <Route path="/home" element={<Homepage/>}/>
+          <Route path="/" element={LoggedIn ? <Homepage /> : <LoginPage />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/:channelId/chattingpage" element={<ChattingPage />} />
+          <Route path="/:channelId/mappage/" element={<Homepage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/home" element={<Homepage />} />
         </Routes>
       </BrowserRouter>
     </div>
