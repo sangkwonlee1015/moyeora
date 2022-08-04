09d6bf34fff0a0d8348666674dfc421c1d7eb496
이상권 <copeve@naver.com>
2022-08-04 19:09:37 +0900

    [FE] feat: 네이버 맵, 클릭시 핀 찍기 구현

--- front/src/components/MapArea.js
+++ front/src/components/MapArea.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps';
+import React from "react";
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from "react-naver-maps";
 
 // function NaverMapComponent() {
 //     const id = this.props.itemData.id;
@@ -17,25 +17,89 @@ import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps';
 //     );
 // }
 
+// function MapArea() {
+//   const YOUR_CLIENT_ID = "qsmjpz291d";
+//   const navermaps = window.naver.maps;
 
-function MapArea(){
-    const YOUR_CLIENT_ID = 'qsmjpz291d'
+//   return (
+//     <div className="maparea">
+//       <RenderAfterNavermapsLoaded clientId={YOUR_CLIENT_ID}>
+//         <NaverMap
+//           mapDivId={"maps-getting-started-uncontrolled"} // default: react-naver-map
+//           style={{
+//             width: "100%",
+//             height: "100%",
+//           }}
+//           defaultCenter={{ lat: 37.5013068, lng: 127.0396597 }}
+//           defaultZoom={18}
+//         //   onClick={function (e) {
+//         //     console.log(e.coord);
+//         //   }}
+//         >
+//           <Marker
+//             position={new navermaps.LatLng(37.5013068, 127.0396597)}
+//             onClick={() => {
+//               alert("여기는 멀티캠퍼스 입니다.");
+//             }}
+//           />
+//         </NaverMap>
+//       </RenderAfterNavermapsLoaded>
+//     </div>
+//   );
+// }
 
-    return(
-        <div className="maparea">
-            <RenderAfterNavermapsLoaded
-            clientId={YOUR_CLIENT_ID}>
-                <NaverMap 
-                mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+const YOUR_CLIENT_ID = "qsmjpz291d";
+
+class MapArea extends React.Component {
+  state = {
+    markers: [{ id: 1, lat: 36.0208521, lng: 129.3578551 }],
+  };
+
+  handleMapClick = (e) => {
+    console.log(this.state);
+    const lat = e.latlng.lat();
+    const lng = e.latlng.lng();
+    this.setState((state) => ({
+      markers: [...state.markers, { id: +new Date(), lat, lng }],
+    }));
+  };
+
+  render() {
+    const { markers } = this.state;
+    const navermaps = window.naver.maps;
+
+    return (
+      <div className="maparea">
+        <RenderAfterNavermapsLoaded clientId={YOUR_CLIENT_ID}>
+          <NaverMap
+            mapDivId={"maps-getting-started-uncontrolled"} // default: react-naver-map
             style={{
-                width: '100%',
-                height: '100%',
+              width: "100%",
+              height: "100%",
             }}
             defaultCenter={{ lat: 37.5013068, lng: 127.0396597 }}
-            defaultZoom={18}/>
-            </RenderAfterNavermapsLoaded>
-        </div>
-    )
+            defaultZoom={18}
+            onClick={this.handleMapClick}
+          >
+            {markers.map((marker) => {
+              return (
+                <Marker
+                  key={marker.id}
+                  position={new navermaps.LatLng(marker.lat, marker.lng)}
+                />
+              );
+            })}
+            <Marker
+              position={new navermaps.LatLng(37.5013068, 127.0396597)}
+              onClick={() => {
+                alert("여기는 멀티캠퍼스 입니다.");
+              }}
+            />
+          </NaverMap>
+        </RenderAfterNavermapsLoaded>
+      </div>
+    );
+  }
 }
 
-export default MapArea
+export default MapArea;
