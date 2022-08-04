35a06acb6d062f51222167f3dee4e484793c1d20
jy-lee0626 <lklll369@gmail.com>
2022-08-04 10:05:28 +0900

    modified_merge_issue

--- front/src/container/App.js
+++ front/src/container/App.js
@@ -1,34 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-<<<<<<< HEAD
-import './App.css';
-import Homepage from './Homepage';
-<<<<<<< HEAD
-import LoginPage from './LoginPage';
-=======
 import "./App.css";
-import Homepage from "./Homepage";
-import LoginPage from "./LoginPage";
->>>>>>> 6934999e9e74a8d3e8670701f47f087a02545f7e
-import Header from "../components/header/Header";
-import ChattingPage from "../components/Sidebar/Chatting";
-
-function App() {
-  let LoggedIn = false;
-  return (
-    <div>
-      {LoggedIn ? <Header /> : null}
-      <BrowserRouter>
-        <Routes>
-<<<<<<< HEAD
-          <Route path='/' element={LoggedIn ? <Homepage/> : <LoginPage/>}/>
-          <Route path="/homepage" element={<Homepage/>}/>
-          <Route path="/:channelId/chattingpage" element={<ChattingPage/>}/>
-          <Route path="/:channelId/mappage/" element={<Homepage/>}/>
-          <Route path="/home" element={<Homepage/>}/>
-          <Route path="/home" element={<Homepage/>}/>
-          <Route path="/home" element={<Homepage/>}/>
-=======
+import Homepage from './Homepage';
 import ChannelHomePage from "./ChannelHomePage";
 import AddChannelPage from "./AddChannelPage";
 import ArticlePage from "./ArticlePage";
@@ -56,19 +29,10 @@ function App() {
           <Route path="/chattingpage" element={<ChattingPage/>}/>
           <Route path="/mappage" element={<MapPage/>}/>
           <Route path="/ovpage" element={<OvPage/>}/>
->>>>>>> front-temp
-=======
-          <Route path="/" element={LoggedIn ? <Homepage /> : <LoginPage />} />
-          <Route path="/homepage" element={<Homepage />} />
-          <Route path="/:channelId/chattingpage" element={<ChattingPage />} />
-          <Route path="/:channelId/mappage/" element={<Homepage />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/home" element={<Homepage />} />
->>>>>>> 6934999e9e74a8d3e8670701f47f087a02545f7e
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
 export default App;
--- front/src/container/Homepage.js
+++ front/src/container/Homepage.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import Header from "../components/header/Header";
-import Home from "../components/Home";
-=======
 import Header from "../components/Header/Header";
 import Home from "../components/Home/Home";
->>>>>>> front-temp
 import React, { useState } from 'react';
 import './Homepage.css'
 
@@ -13,15 +8,7 @@ export default function Homepage(){
     return (
         <div className="comp">
             <Header/>
-<<<<<<< HEAD
-            <Home/>
-=======
-<<<<<<<< HEAD:front/src/container/homepage.js
-            <Home/>
-========
             <Home className="intro"/>
->>>>>>>> front-temp:front/src/container/Homepage.js
->>>>>>> front-temp
         </div>
     );
 }
