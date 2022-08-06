f195bdbdcb343d7ed5ad23b4d9ddda159c5d53d9
이상권 <copeve@naver.com>
2022-08-07 00:31:06 +0900

    [FE] feat: websocket

--- front/src/components/Header/ChannelTest.js
+++ front/src/components/Header/ChannelTest.js
@@ -1,26 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import SockJs from "sockjs-client";
+import StompJs from "stompjs";
 
 //redux
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-export default function ChannelTest(){
-
+export default function ChannelTest() {
   const user = useSelector((store) => store.userReducer.user);
-  const channelList = user.userServer
+  const channelList = user.userServer;
 
-    return (
-      <>
-        <ul className='header_items'>
-          {channelList.map(channel => (
-            <li key={channel.channelId}>
-              <Link to={`/serverpage/${channel.channelId}`}>{ channel.channelName }
-                <span class="tooltiptext">{channel.channelName}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </>
-    );
+  const enterChannel = (id) => {
+    console.log(id);
+    const sock = new SockJs("http://localhost:8080/ws");
+    const stomp = StompJs.over(sock);
+
+    console.log(stomp);
+
+    stomp.connect({}, () => {
+      stomp.subscribe("/user/" + id + "/private", (data) => {
+        const message = JSON.parse(data.body);
+      });
+    });
+  };
+
+  return (
+    <>
+      <ul className="header_items">
+        {channelList.map((channel) => (
+          <li key={channel.channelId}>
+            <Link
+              to={`/serverpage/${channel.channelId}`}
+              onClick={() => enterChannel(channel.channelId)}
+            >
+              {channel.channelName}
+              <span class="tooltiptext">{channel.channelName}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
