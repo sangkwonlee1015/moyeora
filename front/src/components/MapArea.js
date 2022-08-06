9bd786b1d979959ae7da6f8f3f213750fcc279c8
이상권 <copeve@naver.com>
2022-08-06 20:50:03 +0900

    [FE] feat: kakao map 추가

---
+++ front/src/components/MapArea.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+
+function MapArea() {
+  const [markers, setMarkers] = useState([]);
+  return (
+    <div
+      style={{
+        width: "100%",
+      }}
+    >
+      <Map // 지도를 표시할 Container
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        level={5} // 지도의 확대 레벨
+        onClick={(_t, mouseEvent) => {
+          setMarkers([
+            ...markers,
+            {
+              title: "test",
+              lat: mouseEvent.latLng.getLat(),
+              lng: mouseEvent.latLng.getLng(),
+              isInfoOpen: false,
+            },
+          ]);
+          console.log(markers);
+        }}
+      >
+        {markers.map((marker, index) => (
+          <MapMarker
+            key={`${index}`}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            title={`${marker.title}-${index}`}
+            onClick={() => {}}
+          >
+            {<div>{index}</div>}
+          </MapMarker>
+        ))}
+      </Map>
+    </div>
+  );
+}
+
+export default MapArea;
