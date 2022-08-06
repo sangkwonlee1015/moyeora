f45de88ad515af3e7f3a1ec288c92b5036a645c3
이상권 <copeve@naver.com>
2022-08-07 06:24:56 +0900

    [BE] feat: web socket 메시지 주고 받기

--- backend/src/main/java/com/ssafy/api/controller/ChatMessageController.java
+++ backend/src/main/java/com/ssafy/api/controller/ChatMessageController.java
@@ -3,12 +3,16 @@ package com.ssafy.api.controller;
 import com.ssafy.api.request.PinRegisterPostReq;
 import com.ssafy.api.service.ChatService;
 import com.ssafy.api.service.PinService;
+import com.ssafy.common.model.response.BaseResponseBody;
 import com.ssafy.db.entity.ChatMessage;
 import com.ssafy.db.entity.Pin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import springfox.documentation.annotations.ApiIgnore;
 
 
 @Controller
@@ -26,8 +30,16 @@ public class ChatMessageController {
     }
 
     @MessageMapping("/private-message")
-    public void receivePrivateMessage(@Payload ChatMessage message) {
-
+    public void receivePrivateMessage(@ApiIgnore Authentication authentication, @Payload ChatMessage message) {
+        // 로그인 구현 후 테스트
+//        if (authentication == null) {
+//            return ;
+//        }
+        PinRegisterPostReq req = new PinRegisterPostReq();
+        req.setPinLat(message.getLatitude());
+        req.setPinLng(message.getLongitude());
+//        Pin pin = pinService.registerPin(req, );
+        chatService.sendMessagePrivate(message);
     }
 
 }
