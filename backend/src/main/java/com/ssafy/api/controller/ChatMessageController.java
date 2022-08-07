5c425b81dfe61ec6f17bb1ef2aa9997bf8d44156
이상권 <copeve@naver.com>
2022-08-07 15:22:53 +0900

    [BE] feat: pin 메모 메시지 주고 받기

--- backend/src/main/java/com/ssafy/api/controller/ChatMessageController.java
+++ backend/src/main/java/com/ssafy/api/controller/ChatMessageController.java
@@ -1,11 +1,14 @@
 package com.ssafy.api.controller;
 
 import com.ssafy.api.request.PinRegisterPostReq;
+import com.ssafy.api.request.PinUpdatePatchReq;
 import com.ssafy.api.service.ChatService;
 import com.ssafy.api.service.PinService;
+import com.ssafy.common.auth.SsafyUserDetails;
 import com.ssafy.common.model.response.BaseResponseBody;
 import com.ssafy.db.entity.ChatMessage;
 import com.ssafy.db.entity.Pin;
+import com.ssafy.db.entity.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -35,11 +38,30 @@ public class ChatMessageController {
 //        if (authentication == null) {
 //            return ;
 //        }
-        PinRegisterPostReq req = new PinRegisterPostReq();
-        req.setPinLat(message.getLatitude());
-        req.setPinLng(message.getLongitude());
-//        Pin pin = pinService.registerPin(req, );
-        chatService.sendMessagePrivate(message);
+        switch (message.getStatus()){
+            case ADDPIN:
+                PinRegisterPostReq pinRegisterPostReq = new PinRegisterPostReq();
+                pinRegisterPostReq.setPinLat(message.getLat());
+                pinRegisterPostReq.setPinLng(message.getLng());
+                pinRegisterPostReq.setPinContent(message.getPinContent());
+                pinRegisterPostReq.setPinColor(message.getPinColor());
+//                pinRegisterPostReq.setMapSeq(Long.parseLong(message.getMapSeq()));
+//                Long userSeq = ((SsafyUserDetails) authentication.getDetails()).getUser().getUserSeq();
+//                Pin pin = pinService.registerPin(pinRegisterPostReq, userSeq);
+//                message.setPinSeq(pin.getPinSeq().toString());
+                message.setPinSeq(Integer.toString((int)(Math.random()*1000)));
+                chatService.sendMessagePrivate(message);
+                break;
+            case MODPIN:
+                PinUpdatePatchReq pinUpdatePatchReq = new PinUpdatePatchReq();
+                pinUpdatePatchReq.setPinSeq(Long.parseLong(message.getPinSeq()));
+                pinUpdatePatchReq.setPinColor(message.getPinColor());
+                pinUpdatePatchReq.setPinContent(message.getPinContent());
+//                pinService.updatePin(pinUpdatePatchReq);
+                chatService.sendMessagePrivate(message);
+                break;
+
+        }
     }
 
 }
