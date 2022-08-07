102237ff48ba3ad26fc996b13d957a871939fede
KONKUK\s_yjs010604 <didnlie@gmail.com>
2022-08-07 15:13:09 +0900

    [BE] feat: api 수정

--- backend/src/main/java/com/ssafy/api/request/ParticipantsRegisterPostReq.java
+++ backend/src/main/java/com/ssafy/api/request/ParticipantsRegisterPostReq.java
@@ -10,6 +10,5 @@ import lombok.Setter;
 @ApiModel("ParticipantsCreatePostReq")
 public class ParticipantsRegisterPostReq {
     private Long channelSeq;
-    private Long userSeq;
     private String channelPassword;
 }
--- backend/src/test/java/com/ssafy/api/service/ParticipantsServiceImplTest.java
+++ backend/src/test/java/com/ssafy/api/service/ParticipantsServiceImplTest.java
@@ -15,20 +15,4 @@ class ParticipantsServiceImplTest {
     @Autowired
     ParticipantsService participantsService;
 
-    @Test
-    public void testing(){
-        ParticipantsId participantsId = new ParticipantsId();
-        participantsId.setUserId("didnlie");
-        participantsId.setRoomSeq(1L);
-
-        ParticipantsRegisterPostReq participantsRegisterPostReq = new ParticipantsRegisterPostReq();
-        participantsRegisterPostReq.setRoomSeq(1L);
-        participantsRegisterPostReq.setUserId("didnlie");
-
-        participantsService.deleteParticipants(participantsId);
-
-        Participants p = participantsService.registerParticipants(participantsRegisterPostReq);
-
-        Assertions.assertThat(participantsService.getParticipantsById(participantsId).get()).isEqualTo(p);
-    }
 }
