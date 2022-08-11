d95c4df9d3157800eb483f47865666f10e0b1a76
윤경식 <didnlie@naver.com>
2022-08-11 12:56:33 +0900

    Deleted front/public/favicon.ico, front/public/index.html, front/public/logo192.png, front/public/logo512.png, front/public/manifest.json, front/public/robots.txt, front/src/api/auth.js, front/src/api/channel.js, front/src/api/index.js, front/src/api/map.js, front/src/api/participant.js, front/src/api/pin.js, front/src/api/user.js, front/src/assets/add.png, front/src/assets/find.png, front/src/assets/home.png, front/src/assets/setting.jpg, front/src/components/Article/Article.js, front/src/components/Article/ArticleList.js, front/src/components/Header/ChannelTest.js, front/src/components/Header/Header.css, front/src/components/Header/Header.js, front/src/components/Home/Home.css, front/src/components/Home/Home.js, front/src/components/Mypage/MyMap.js, front/src/components/Mypage/MypageSetting.js, front/src/components/Sidebar/Chatting.js, front/src/components/Sidebar/Map.js, front/src/components/Sidebar/MypageSidebar.js, front/src/components/Sidebar/OV.js, front/src/components/Sidebar/Sidebar.css, front/src/components/Sidebar/Sidebar.js, front/src/components/AddChannel.js, front/src/components/ChannelHome.js, front/src/components/ChattingArea.js, front/src/components/EditorComponent.js, front/src/components/MapArea.js, front/src/components/MapMarkerList.css, front/src/components/MapMarkerList.js, front/src/components/OvArea.css, front/src/components/OvArea.js, front/src/container/AddChannelPage.js, front/src/container/App.css, front/src/container/App.js, front/src/container/App.test.js, front/src/container/ArticlePage.js, front/src/container/ChannelHomePage.js, front/src/container/ChattingPage.js, front/src/container/Homepage.css, front/src/container/Homepage.js, front/src/container/LoginPage.js, front/src/container/MainPage.css, front/src/container/MapPage.js, front/src/container/MypageMapPage.js, front/src/container/MypageSettingPage.js, front/src/container/OVPage.js, front/src/redux/ChannelList.js, front/src/redux/MapList.js, front/src/redux/PinList.js, front/src/redux/UserInfo.js, front/src/redux/store.js, front/src/index.css, front/src/index.js, front/src/logo.svg, front/src/reportWebVitals.js, front/src/setupTests.js, front/package-lock.json, front/package.json, front/setupProxy.js, front/testbranch.txt

--- front/src/components/AddChannel.js
+++
@@ -1,7 +0,0 @@
-function AddChannel(){
-  return (
-    <div className="addchannel">방 생성 page입니다.</div>
-  );
-}
-
-export default AddChannel
--- front/src/components/ChattingArea.js
+++
@@ -1,7 +0,0 @@
-function ChattingArea(){
-  return(
-    <div className="chattingarea">채팅이 보여지는 화면입니다.</div>
-  )
-}
-
-export default ChattingArea
