5c55b770f1e9a985c7b533a6c3dcddab9db13ce6
SSAFY <ygeum1@gmail.com>
2022-08-14 18:29:55 +0900

    mypage_sidebar_map

--- backend/src/main/frontend/src/components/Mypage/Map.js
+++ backend/src/main/frontend/src/components/Mypage/Map.js
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 import { getPinList } from "../../api/pin";
+import { useState } from "react";
+
 import { useSelector, useDispatch } from "react-redux";
 import { SET_CURRENTMAP } from "../../redux/PinList";
 import { SET_PINLIST } from "../../redux/PinList";
+import { deleteMap, getMapList } from "../../api/map";
+import { SET_MAPLIST } from "../../redux/MapList";
 // import "./Sidebar.css";
 
 function Map(props) {
   const token = useSelector((state) => state.UserInfo.accessToken);
   const dispatch = useDispatch();
   const ChannelSeq = useSelector((state) => state.ChannelList.channelSeq);
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
 
   function openMap(mapSeq) {
     dispatch(SET_CURRENTMAP(mapSeq));
@@ -28,7 +35,26 @@ function Map(props) {
     );
   }
 
-  const DeleteMap = () => console.log("맵 삭제");
+  const DeleteMap = () => {             // 맵 삭제 함수
+    const mapSeq = props.mapSeq
+    // 맵삭제
+    deleteMap(
+      mapSeq, 
+      token,
+      getMapList(                       // 성공하면 맵 리스트 불러와서
+        1,
+        "user",
+        token,
+        (response) => {                 // 삭제한 맵이 response에 담겨옴 (문제 발생)
+          const list = response.data.mapsList;
+          const arr = list.filter(data => data.mapSeq !== mapSeq);  // fliter로 해결!
+          dispatch(SET_MAPLIST(arr));  // 맵 redux에 다시 저장
+          handleClose();
+        },
+      ), 
+      (error) => console.log(error)     // 실패하면
+    )
+  }
   return (
     <div>
       <span>
