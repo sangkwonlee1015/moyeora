bf1654d9b646c45b7f56519dcd78cc1da110f7eb
SSAFY <ygeum1@gmail.com>
2022-08-16 18:00:04 +0900

    css: part_list

--- backend/src/main/frontend/src/components/ChannelHome.js
+++ backend/src/main/frontend/src/components/ChannelHome.js
@@ -3,10 +3,26 @@ import {
   getParticipantListByChannel,
 } from "../api/participant";
 import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import ChannelParticipant from "./ChannelParticipant";
-import { getChannelInfo } from "../api/channel";
+import { deleteChannel, getChannelInfo } from "../api/channel";
+import "./ChannelHome.css"
+
+// mui
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import TextField from '@mui/material/TextField';
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
 
 function ChannelHome(props) {
   const channelSeq = useSelector((state) => state.ChannelList.channelSeq); //33
@@ -18,7 +34,6 @@ function ChannelHome(props) {
   const [channelDesc, setChannelDesc] = useState();
   const [channelName, setChannelName] = useState();
   const [channelTag, setChannelTag] = useState();
-
 
   console.log(pList);
 
@@ -53,33 +68,43 @@ function ChannelHome(props) {
   }, [channelSeq]);
 
   return (
-    <div className="server-home">
-      <h2>서버홈</h2>
-
-      <br />
-      채널 이름 {channelName} {'('}#{channelSeq}{')'}
-      <br />
-      채널 설명 {channelDesc}
-      <br />
-      채널 태그 {channelTag}
-
-      <br />
-      <br />
-      <br />
-
-
-      <div>
+    <div className="channel-home">
+      {/* <h2>서버홈</h2> */}
+      <div className="channel-background">
+        <h2>채널 소개</h2>
         <br />
-        현재 채널 참여자 목록:{" "}
-        {pList.map((item, index) => {
-          return (
-            <ChannelParticipant
-              key={index}
-              userSeq={item.participantsId.userSeq}
-            ></ChannelParticipant>
-          );
-        })}
+        채널 이름 {channelName} {'('}#{channelSeq}{')'}
+        <br />
+        채널 설명 {channelDesc}
+        <br />
+        채널 태그 {channelTag}
 
+        <br />
+        <br />
+        <br />
+
+
+        <div>
+          <br />
+          <h3>현재 채널 참여자 목록:{" "}</h3>
+          {pList.map((item, index) => {
+            return (
+
+              <div className="abc">
+                <ChannelParticipant className="def"
+                  key={index}
+                  userSeq={item.participantsId.userSeq}
+                ></ChannelParticipant>
+              </div>
+
+
+            );
+          })}
+        </div>
+
+        
+
+        
       </div>
     </div>
   );
